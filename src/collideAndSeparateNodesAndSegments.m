3b5db430e76db9b713dcd5a45d0d8b6920bba81e
Daniel Celis Garza <dcelisgarza@users.noreply.github.com>
2021-03-16 17:13:49 +0000

    Forgot to add changes to the standalone collision.

--- src/collideAndSeparateNodesAndSegments.m
+++ src/collideAndSeparateNodesAndSegments.m
@@ -60,7 +60,6 @@ function [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew] = collid
                         %                             s2Skip = [];
                         %                         end
 
-                        continue
                     else
 
                         if floop == 1
@@ -91,29 +90,29 @@ function [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew] = collid
         % that should be performed, here we just collide them and the
         % outside separation takes care of them.
         colliding_segments = 1;
-
+        s1Skip = [];
+        s2Skip = [];
         while colliding_segments == 1
             [colliding_segments, n1s1, n2s1, n1s2, n2s2, floop, s1, s2, segpair] = CollisionCheckerMex(rnnew(:, 1), rnnew(:, 2), rnnew(:, 3), rnnew(:, end), ...
-                rnnew(:, 4), rnnew(:, 5), rnnew(:, 6), linksnew(:, 1), linksnew(:, 2), connectivitynew, rann, linksnew(:, 3), linksnew(:, 4), linksnew(:, 5), linksnew(:, 6), linksnew(:, 7), linksnew(:, 8), [], []);
+                rnnew(:, 4), rnnew(:, 5), rnnew(:, 6), linksnew(:, 1), linksnew(:, 2), connectivitynew, rann, linksnew(:, 3), linksnew(:, 4), linksnew(:, 5), linksnew(:, 6), linksnew(:, 7), linksnew(:, 8), s1Skip, s2Skip);
 
-            if colliding_segments == 1%scan and update dislocation structure.
-
-                if floop == 1
-                    fprintf("Step %d. Unconnected links found. Links %d and %d are colliding.\n", curstep, s1, s2)
-                elseif floop == 2
-                    fprintf("Step %d. Links %d and %d colliding by hinge condition.\n", curstep, s1, s2)
-                end
-
-                if colliding_segments == 1
-
-                    [rnnew, linksnew, ~, ~, fsegnew, colliding_segments, ~] = collision(rnnew, linksnew, connectivitynew, ...
-                        linksinconnectnew, fsegnew, rann, MU, NU, a, Ec, mobility, vertices, rotMatrix, u_hat, nc, xnodes, ...
-                        D, mx, my, mz, w, h, d, floop, n1s1, n2s1, n1s2, n2s2, s1, s2, segpair, lmin, CUDA_flag, Bcoeff);
-
+            if colliding_segments == 1
+                [rnnew, linksnew, ~, ~, fsegnew, colliding_segments, collided] = collision(rnnew, linksnew, connectivitynew, ...
+                    linksinconnectnew, fsegnew, rann, MU, NU, a, Ec, mobility, vertices, rotMatrix, u_hat, nc, xnodes, ...
+                    D, mx, my, mz, w, h, d, floop, n1s1, n2s1, n1s2, n2s2, s1, s2, segpair, lmin, CUDA_flag, Bcoeff);
+                
+                if collided
                     %removing links with effective zero Burgers vectors
                     [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew] = cleanupsegments(rnnew, linksnew, fsegnew);
+                    if floop == 1
+                        fprintf("Step %d. Unconnected links found. Links %d and %d are colliding.\n", curstep, s1, s2)
+                    elseif floop == 2
+                        fprintf("Step %d. Links %d and %d colliding by hinge condition.\n", curstep, s1, s2)
+                    end
+                else
+                    s1Skip = [s1Skip; s1];
+                    s2Skip = [s2Skip; s2];
                 end
-
             end
 
         end
