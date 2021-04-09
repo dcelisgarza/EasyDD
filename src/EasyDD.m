f2899a4357e5b690f88a58668da5f2a9ba7a1290
brucebromage <32520919+brucebromage@users.noreply.github.com>
2021-04-09 22:19:49 +0100

    Cantilever fix and variable updates

--- src/EasyDD.m
+++ src/EasyDD.m
@@ -84,7 +84,7 @@ plotFEMDomain(Stop, Sbot, Sright, Sleft, Sfront, Sback, Smixed, xnodes)
         idxi, n_nodes_t, n_threads, para_scheme, gamma_disp, u_tilda_0, ...
         u, u_hat, u_tilda] = AuxFEMCoupler(mno, dx, dy, dz, mx, my, mz, ...
     xnodes, nc, gammat, gammau, gammaMixed, calculateTractions, CUDA_flag, ...
-    n_threads, para_scheme);
+    n_threads, para_scheme, simType);
 
 % Use Delaunay triangulation to create surface mesh, used for visualisation
 % dislocation remeshing algorithm.
@@ -100,17 +100,16 @@ plotFEMDomain(Stop, Sbot, Sright, Sleft, Sfront, Sback, Smixed, xnodes)
 u_tilda_0 = calculateUtilda(rn, links, gamma_disp, NU, xnodes, dx, ...
     dy, dz, mx, my, mz, u_tilda_0);
 close all
-save(sprintf('../output/initial_%s_%d', simName, curstep), 'K', 'kg', 'L', 'U', 'P_l', 'P_u');
+save(sprintf('../output/initial_%s_%d', simName, curstep), 'K','kg','L','U','P_l','P_u');
 
 fprintf('Initialisation complete.\n');
 %%
 while simTime < totalSimTime
-
     if ~any(rn(:, 4) == 0)
         fprintf('No more real segments, ending simulation.\n')
         return;
     end
-
+    
     if mod(curstep, saveFreq) == 0
         close all
         save(sprintf('../output/%s_%d', simName, curstep), '-regexp', '^(?!(K|kg|L|U|P_l|P_u)$).');
@@ -151,15 +150,15 @@ while simTime < totalSimTime
         mobility, rotMatrix, doremesh, dovirtmesh, vertices, u_hat, nc, xnodes, D, mx, my, mz, w, h, d, ...
         TriangleCentroids, TriangleNormals, CUDA_flag, Bcoeff, noExitNorm, noExitPoint);
 
-    [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew] = collideAndSeparateNodesAndSegments(docollision, doseparation, ...
+   [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew] = collideAndSeparateNodesAndSegments(docollision, doseparation, ...
         rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew, rann, MU, NU, a, Ec, mobility, vertices, rotMatrix, ...
         u_hat, nc, xnodes, D, mx, my, mz, w, h, d, lmin, CUDA_flag, Bcoeff, curstep);
 
     rnnew = fixBlockadingNodes(rnnew, connectivitynew);
 
-    [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew, ~, ~] = separation(doseparation, rnnew, ...
+    [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew, ~] = separation(doseparation, rnnew, ...
         linksnew, connectivitynew, linksinconnectnew, fsegnew, mobility, rotMatrix, MU, NU, a, Ec, ...
-        2 * rann, vertices, u_hat, nc, xnodes, D, mx, my, mz, w, h, d, CUDA_flag, Bcoeff, 0);
+        2 * rann, vertices, u_hat, nc, xnodes, D, mx, my, mz, w, h, d, CUDA_flag, Bcoeff,0);
 
     [rnnew, linksnew, connectivitynew, linksinconnectnew, fsegnew] = remesh_all(rnnew, linksnew, ...
         connectivitynew, linksinconnectnew, fsegnew, lmin, lmax, areamin, areamax, MU, NU, a, Ec, ...
